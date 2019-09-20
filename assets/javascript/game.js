aa7da19fdd2d91cf483d222ae0b07ec332842b81
Lucio Verane <verane64@gmail.com>
2019-09-20 16:24:22 -0500

    some for loops replaced by indexOf in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,16 @@
 var animals = ["giraffe", "rhino", "zebra", "hyena", "buffalo"];
 chosenAnimal = animals[Math.floor(Math.random() * animals.length)];
-var splitChosenAnimal = [];
 //chosenAnimal and splitChosenAnimal should be the same length
+var splitChosenAnimal = [];
 splitChosenAnimal = chosenAnimal.split("");
 var wordToBeGuessed = [];
 
-
 var guessesLeft = 12;
 var guessedLetters = [];
-var alreadyUsed = false;
 
 var wordAsString;
-
-var correctLetter = false;
-
+// var guessedAsString;
+// guessedAsString = guessedLetters.join(" ");
 
 console.log(chosenAnimal);
 console.log(splitChosenAnimal);
@@ -21,49 +18,41 @@ console.log(chosenAnimal.length);
 console.log(splitChosenAnimal.length);
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    //guesses.textContent = "12"; //shouldnt be here
     for(i=0; i<splitChosenAnimal.length; i++){
         wordToBeGuessed[i] = "_";
     }
-    console.log("word to be guessed" + wordToBeGuessed);
     wordAsString = wordToBeGuessed.join(" ")
-    console.log(wordAsString);
-    emptyWord.textContent = wordAsString;
+    emptyWord.textContent = wordAsString;//this and up can be a function
     document.onkeydown = function(event) {
         userGuess = event.key;
-        for (i=0; i<=guessedLetters.length; i++){//maybe do indexOf() instead
-            if(userGuess === guessedLetters[i]){
-                alreadyUsed = true;
-                finalMessage.textContent = "You already tried that";//use a variable that switches between true/false and do that check outside the for loop
-            // }else{
-            //     guessedLetters[guessedLetters.length] = userGuess; //use a variable that switches between true/false and do that check outside the for loop
-            //     usedLetters.textContent = guessedLetters; //append here
-            }
-        }
-
-        if(alreadyUsed === false){
-            // usedLetters is stacking forever
+        console.log(guessedLetters.indexOf(userGuess));
+        console.log(guessedLetters);
+        console.log(userGuess);
+        if(guessedLetters.indexOf(userGuess) === -1){
+            console.log("test");
+            finalMessage.textContent = "";
             guessedLetters[guessedLetters.length] = userGuess;
             usedLetters.textContent = guessedLetters;
-            for(i=0; i<chosenAnimal.length; i++){
-                if(userGuess === splitChosenAnimal[i]){
+
+            if(chosenAnimal.indexOf(userGuess) === -1){
+            
+                guessesLeft--;
+                if(guessesLeft === 0){
+                    finalMessage.textContent = "You lost";
+                }
+                guesses.textContent = guessesLeft;
+            }else{
+                for(i=0; i<chosenAnimal.length; i++){
+                    if(userGuess === splitChosenAnimal[i]){
                     wordToBeGuessed[i] = userGuess;
-                    correctLetter = true;
+                    }
                 }
             }
             wordAsString = wordToBeGuessed.join(" ")
             emptyWord.textContent = wordAsString;
-            // if(correctLetter === true){
-            //     correctLetter === false;
-            // }else{
-                guessesLeft = guessesLeft - 1;
-            // }
-            if(guessesLeft === 0){
-                finalMessage.textContent = "You lost";
-            }
-            guesses.textContent = guessesLeft;
+
         }else{
-            alreadyUsed = false;
+            finalMessage.textContent = "You already tried that";
         }
     }
 });
