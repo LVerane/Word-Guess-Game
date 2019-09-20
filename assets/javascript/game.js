a0d64d20b8ec5b7744dd99b27d06d4a91e0d622b
Lucio Verane <verane64@gmail.com>
2019-09-19 23:45:29 -0500

    js check for 'guessedLetters' fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,69 @@
 var animals = ["giraffe", "rhino", "zebra", "hyena", "buffalo"];
+chosenAnimal = animals[Math.floor(Math.random() * animals.length)];
+var splitChosenAnimal = [];
+//chosenAnimal and splitChosenAnimal should be the same length
+splitChosenAnimal = chosenAnimal.split("");
+var wordToBeGuessed = [];
+
+
 var guessesLeft = 12;
 var guessedLetters = [];
-// var alreadyUsed = false;
+var alreadyUsed = false;
 
-var split = [];
-var wordToBeGuessed = [];
 var wordAsString;
 
 var correctLetter = false;
 
-chosenAnimal = animals[Math.floor(Math.random() * animals.length)];
-console.log(chosenAnimal);
-split = chosenAnimal.split("");
-console.log(split);
-console.log(chosenAnimal.length);
-console.log(split.length);
 
+console.log(chosenAnimal);
+console.log(splitChosenAnimal);
+console.log(chosenAnimal.length);
+console.log(splitChosenAnimal.length);
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    for(i=0; i<chosenAnimal.length; i++){
+    //guesses.textContent = "12"; //shouldnt be here
+    for(i=0; i<splitChosenAnimal.length; i++){
         wordToBeGuessed[i] = "_";
     }
+    console.log("word to be guessed" + wordToBeGuessed);
     wordAsString = wordToBeGuessed.join(" ")
     console.log(wordAsString);
-    empty.textContent = wordAsString;
+    emptyWord.textContent = wordAsString;
     document.onkeydown = function(event) {
         userGuess = event.key;
-        // for (i=0; i<=guessedLetters.length; i++){
-        //     if(userGuess === guessedLetters[i]){
-        //         finalMessage.textContent = "You already tried that";
-        //     }else{
-        //         guessedLetters[guessedLetters.length] = userGuess;
-        //         usedLetters.textContent = guessedLetters;
-        //     }
-        // }
-        // if(alreadyUsed === false){
+        for (i=0; i<=guessedLetters.length; i++){//maybe do indexOf() instead
+            if(userGuess === guessedLetters[i]){
+                alreadyUsed = true;
+                finalMessage.textContent = "You already tried that";//use a variable that switches between true/false and do that check outside the for loop
+            // }else{
+            //     guessedLetters[guessedLetters.length] = userGuess; //use a variable that switches between true/false and do that check outside the for loop
+            //     usedLetters.textContent = guessedLetters; //append here
+            }
+        }
+
+        if(alreadyUsed === false){
+            // usedLetters is stacking forever
+            guessedLetters[guessedLetters.length] = userGuess;
+            usedLetters.textContent = guessedLetters;
             for(i=0; i<chosenAnimal.length; i++){
-                if(userGuess === split[i]){
+                if(userGuess === splitChosenAnimal[i]){
                     wordToBeGuessed[i] = userGuess;
                     correctLetter = true;
                 }
             }
             wordAsString = wordToBeGuessed.join(" ")
-            empty.textContent = wordAsString;
-            if(correctLetter === true){
-                correctLetter === false;
-            }else{
+            emptyWord.textContent = wordAsString;
+            // if(correctLetter === true){
+            //     correctLetter === false;
+            // }else{
                 guessesLeft = guessesLeft - 1;
-            }
+            // }
             if(guessesLeft === 0){
                 finalMessage.textContent = "You lost";
             }
             guesses.textContent = guessesLeft;
-        // }else{
-        //     alreadyUsed = false;
-        // }
+        }else{
+            alreadyUsed = false;
+        }
     }
 });
