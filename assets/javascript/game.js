0de294d22651d30f3d70e0a52a90b170fd073df2
Lucio Verane <verane64@gmail.com>
2019-09-20 18:05:21 -0500

    reset function and win/loss count added to js, background color adjusted in css, win/loss count added to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,91 @@
 var animals = ["giraffe", "rhino", "zebra", "hyena", "buffalo"];
-chosenAnimal = animals[Math.floor(Math.random() * animals.length)];
 //chosenAnimal and splitChosenAnimal should be the same length
-var splitChosenAnimal = [];
-splitChosenAnimal = chosenAnimal.split("");
-var wordToBeGuessed = [];
+var chosenAnimal
+var splitChosenAnimal
 
-var guessesLeft = 12;
-var guessedLetters = [];
+var wordToBeGuessed
+var guessesLeft
+var guessedLetters
+var wrongGuesses
 
-var wordAsString;
-// var guessedAsString;
-// guessedAsString = guessedLetters.join(" ");
+win = 0;
+loss = 0;
 
-console.log(chosenAnimal);
-console.log(splitChosenAnimal);
-console.log(chosenAnimal.length);
-console.log(splitChosenAnimal.length);
+ready = false;
 
-document.addEventListener("DOMContentLoaded", function(event) {
+// console.log(chosenAnimal);
+// console.log(splitChosenAnimal);
+// console.log(chosenAnimal.length);
+// console.log(splitChosenAnimal.length);
+
+function reset (){
+    chosenAnimal = animals[Math.floor(Math.random() * animals.length)];
+    splitChosenAnimal = chosenAnimal.split("");
+    wordToBeGuessed = [];
+    guessesLeft = 12;
+    guessedLetters = [];
+    wrongGuesses = [];
     for(i=0; i<splitChosenAnimal.length; i++){
         wordToBeGuessed[i] = "_";
     }
-    wordAsString = wordToBeGuessed.join(" ")
-    emptyWord.textContent = wordAsString;//this and up can be a function
-    document.onkeydown = function(event) {
-        userGuess = event.key;
-        console.log(guessedLetters.indexOf(userGuess));
-        console.log(guessedLetters);
-        console.log(userGuess);
-        if(guessedLetters.indexOf(userGuess) === -1){
-            console.log("test");
-            finalMessage.textContent = "";
-            guessedLetters[guessedLetters.length] = userGuess;
-            usedLetters.textContent = guessedLetters;
+    emptyWord.textContent = wordToBeGuessed.join(" ");
+    guesses.textContent = guessesLeft;
+    finalMessage.textContent = "";
+    usedLetters.textContent = "";
+}
 
-            if(chosenAnimal.indexOf(userGuess) === -1){
-            
-                guessesLeft--;
-                if(guessesLeft === 0){
-                    finalMessage.textContent = "You lost";
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    document.onkeydown = function(event) {
+        if(ready === false){
+            title.textContent = "Can you figure out the word?";   
+            ready = true;
+            reset();
+        }else{
+            userGuess = event.key;
+
+            if(guessedLetters.indexOf(userGuess) === -1){
+                finalMessage.textContent = "";
+                guessedLetters.push(userGuess);
+                if(chosenAnimal.indexOf(userGuess) === -1){
+                    wrongGuesses.push(userGuess);
                 }
-                guesses.textContent = guessesLeft;
-            }else{
-                for(i=0; i<chosenAnimal.length; i++){
-                    if(userGuess === splitChosenAnimal[i]){
-                    wordToBeGuessed[i] = userGuess;
+                
+                usedLetters.textContent = wrongGuesses.join(" ");
+
+                if(chosenAnimal.indexOf(userGuess) === -1){
+                
+                    guessesLeft--;
+                    if(guessesLeft === 0){
+                        finalMessage.textContent = "Press any key to try again";
+                        title.textContent = "You Lost!";
+                        loss++;
+                        losses.textContent = loss;
+                        ready = false;                        
+                    }
+                    guesses.textContent = guessesLeft;
+                
+                }else{
+                    for(i=0; i<chosenAnimal.length; i++){
+                        if(userGuess === splitChosenAnimal[i]){
+                        wordToBeGuessed[i] = userGuess;
+                        }
                     }
                 }
-            }
-            wordAsString = wordToBeGuessed.join(" ")
-            emptyWord.textContent = wordAsString;
+                emptyWord.textContent = wordToBeGuessed.join(" ");
 
-        }else{
-            finalMessage.textContent = "You already tried that";
+            }else{
+                finalMessage.textContent = "You already tried that";
+            }
+
+            if(wordToBeGuessed.join("") === chosenAnimal){
+                finalMessage.textContent = "Press any key to try again";
+                title.textContent = "Congratulations!! You Won!!";
+                win++;
+                wins.textContent = win;
+                ready = false;
+            }
+
         }
     }
 });
